1d1d12cfe1b3097e4552ae99904065f1bbfa6552
Ana Gomes <aninha.g.souza@hotmail.com>
2020-01-13 12:36:33 -0300

    Add files via upload

---
+++ EntradaFuncao.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>FUNCAO PHP</title>
+		<meta charset="UTF-8">
+	</head>
+	<body>
+		<form action = "Funcao.php" method = "POST">
+			Digite a quantidade de chuva em polegadas: <input type = "number" name = "polegadas">
+			<input type = "submit" value = "Converter em milimetros"> 
+	</body>
+	</body>
+</html>
