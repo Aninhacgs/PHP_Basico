c7702a765a9460408e963e0191e9da4edb5ab4e1
Ana Gomes <aninha.g.souza@hotmail.com>
2020-01-13 12:39:08 -0300

    Add files via upload

---
+++ Variaveis.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>VARIAVEIS PHP</title>
+	</head>
+	<body>
+		<?PHP
+			$Nome = "ANA CLAUDIA";
+			$SobreNome = "GOMES SOUZA";
+			$Idade = 28;
+			echo "<p>Nome: $Nome $SobreNome</p> <p>Idade: $Idade anos</p>";
+		?>
+	</body>
+</html>
