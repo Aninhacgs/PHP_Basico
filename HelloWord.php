20ea916de3b53a587cabaf43606bd9874387821e
Ana Gomes <aninha.g.souza@hotmail.com>
2020-01-13 12:37:46 -0300

    Add files via upload

---
+++ HelloWord.php
@@ -0,0 +1,15 @@
+<!DOCTYPE hmtl>
+<html>
+	<head>
+		<title> PHP BASICO</title>
+		<?PHP
+			$css = "style.css";
+			echo "<link rel='stylesheet' href='" . $css . "'>";
+		?>
+	</head>
+	<body>
+		<?php
+		echo "<p>HELLO WORLD</p>"; 
+		?> 
+	</body>
+</html>
