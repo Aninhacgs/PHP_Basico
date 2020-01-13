cc6f49cce812491f18971528fbbf01c98d5309d7
Ana Gomes <aninha.g.souza@hotmail.com>
2020-01-13 12:37:17 -0300

    Add files via upload

---
+++ formulario.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>FORMULARIO PHP</title>
+		<meta charset="UTF-8">
+	</head>
+	<body>
+		<form  action ="dados.php" method="POST">
+			Nome: <input type = "text" name = "nome">
+			Email: <input type = "email" name= "email">
+			<input  type = "submit" name = "Enviar">
+		</form> 
+	</body>
+</html>
